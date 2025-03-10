a72a0e90a84fce069bcd3f65cc2941f708a1a110
Alyaras <alyarahmadayanis@gmail.com>
2025-03-10 16:08:05 +0700

    menambahkan implementasi dengan table-driven

---
+++ TpMod4/KodePos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpMod4
+{
+    class KodePos
+    {
+        public enum kodePos
+        {
+            Batununggal, Kujangsari, Mengger, Wates, Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru, Maleer,Samoja
+        }
+        public static int getKodePos(kodePos kdPos)
+        {
+            int[] kodePosKelurahan = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+            return kodePosKelurahan[(int)kdPos];
+        }
+    }
+}
