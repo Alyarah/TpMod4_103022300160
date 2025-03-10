e6d8471fed2c2e617e8b75eac3e46eb0c3889bf1
Alyaras <alyarahmadayanis@gmail.com>
2025-03-10 18:48:33 +0700

    menambahkan implementasi dengan state-based construction

---
+++ TpMod4/DoorMachine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpMod4
+{
+    class DoorMachine
+    {
+        public enum pintu { Terkunci, Terbuka };
+        public enum Trigger { KunciPintu, bukaPintu };
+
+        class transition
+        {
+            public pintu prevState;
+            public pintu nextState;
+            public Trigger trigger;
+            public transition(pintu prevState, pintu nextState, Trigger trigger)
+            {
+                this.prevState = prevState;
+                this.nextState = nextState;
+                this.trigger = trigger;
+            }
+        }
+            transition[] transitions ={
+                new transition(pintu.Terkunci, pintu.Terbuka, Trigger.bukaPintu),
+                new transition(pintu.Terkunci, pintu.Terkunci, Trigger.KunciPintu),
+                new transition(pintu.Terbuka, pintu.Terkunci, Trigger.KunciPintu),
+                new transition(pintu.Terbuka, pintu.Terbuka, Trigger.bukaPintu)
+            };
+            public pintu currentState;
+            public pintu getNextState(pintu prevState, Trigger trigger)
+            {
+                pintu nextState = prevState;
+                for (int i = 0; i < transitions.Length; i++)
+                {
+                    if (transitions[i].prevState == prevState && transitions[i].trigger == trigger)
+                        nextState = transitions[i].nextState;
+                }
+                return nextState;
+            }
+            public void activateTrigger(Trigger trigger)
+            {
+                pintu nextState = getNextState(currentState, trigger);
+                currentState = nextState;
+            }
+    }
+
+}
--- TpMod4/Program.cs
+++ TpMod4/Program.cs
@@ -1,2 +1,60 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using TpMod4;
+
+namespace TpMod4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Kelurahan dan Kode Pos");
+            Console.WriteLine("Batununggal = " + KodePos.getKodePos(KodePos.kodePos.Batununggal));
+            Console.WriteLine("Kujangsari =	" + KodePos.getKodePos(KodePos.kodePos.Kujangsari));
+            Console.WriteLine("Mengger = " + KodePos.getKodePos(KodePos.kodePos.Mengger));
+            Console.WriteLine("Wates = " + KodePos.getKodePos(KodePos.kodePos.Wates));
+            Console.WriteLine("Cijaura = " + KodePos.getKodePos(KodePos.kodePos.Cijaura));
+            Console.WriteLine("Jatisari = " + KodePos.getKodePos(KodePos.kodePos.Jatisari));
+            Console.WriteLine("Margasari = " + KodePos.getKodePos(KodePos.kodePos.Margasari));
+            Console.WriteLine("Sekejati = " + KodePos.getKodePos(KodePos.kodePos.Sekejati));
+            Console.WriteLine("Kebonwaru = " + KodePos.getKodePos(KodePos.kodePos.Kebonwaru));
+            Console.WriteLine("Maleer = " + KodePos.getKodePos(KodePos.kodePos.Maleer));
+            Console.WriteLine("Samoja = " + KodePos.getKodePos(KodePos.kodePos.Samoja));
+            Console.WriteLine();
+
+            DoorMachine door = new DoorMachine();
+            door.currentState = DoorMachine.pintu.Terkunci; 
+            Console.WriteLine("Pintu terkunci");
+
+            while (true)
+            {
+                Console.Write("Masukkan state: ");
+                string input = Console.ReadLine().ToLower();
+
+                switch (input)
+                {
+                    case "buka":
+                        door.activateTrigger(DoorMachine.Trigger.bukaPintu);
+                        if (door.currentState == DoorMachine.pintu.Terbuka)
+                            Console.WriteLine("Pintu tidak terkunci");
+                        break;
+
+                    case "kunci":
+                        door.activateTrigger(DoorMachine.Trigger.KunciPintu);
+                        if (door.currentState == DoorMachine.pintu.Terkunci)
+                            Console.WriteLine("Pintu terkunci");
+                        break;
+
+                    case "keluar":
+                        Console.WriteLine("Program selesai.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Perintah tidak valid.");
+                        break;
+                }
+            }
+        }
+    }
+}
+
